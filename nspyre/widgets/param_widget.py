7ce78895c19e99aef8c4c6d17c1e5c398c3f9c70
AlexBourassa <abourassa@uchicago.edu>
2019-11-09 23:32:01 -0600

    Move the RangeDict to utils.py

--- nspyre/widgets/param_widget.py
+++ nspyre/widgets/param_widget.py
@@ -6,52 +6,7 @@ from collections import OrderedDict
 
 import inspect
 from nspyre.widgets.spinbox import SpinBox
-
-
-def arange(start, stop, step, dtype=None):
-    return np.arange(start, stop, step, dtype=dtype)
-
-class RangeDict(dict):
-    FUNCS = {'linspace':np.linspace, 'arange':arange, 'logspace':np.logspace}
-
-    def __init__(self, **initial_dict):
-        self._verify_valid(initial_dict)
-        dict.__setitem__(self, 'func', initial_dict.pop('func'))
-        for k in initial_dict:
-            self[k] = initial_dict[k]
-
-    def _verify_valid(self, d):
-        # Make sure there is a valid func argument
-        if not 'func' in d or d['func'] not in self.FUNCS:
-            raise ValueError('RangeDict must have a element <func> in: {}'.format(self.FUNCS.keys()))
-        
-        # Make sure all required args are define
-        signature = inspect.signature(self.FUNCS[d['func']])
-        for param in signature.parameters.keys():
-            if signature.parameters[param].default == inspect._empty and not param in d:
-                raise ValueError('RangeDict does not define required <{}> argument'.format(param))
-
-    def __setitem__(self, key, val):
-        if key == 'func':
-            raise KeyError('Cannot redefine the func, you should reinstanciate with new parametters')
-        elif key in inspect.signature(self.FUNCS[self['func']]).parameters:
-            return dict.__setitem__(self, key, val)
-
-    @property
-    def array(self):
-        d = self.copy()
-        func_name = d.pop('func')
-        units = None
-        for p in d:
-            if type(d[p])==Q_:
-                if units is None:
-                    units = d[p].units
-                d[p] = d[p].to(units).m
-        if units is None or units == Q_(1, 'dimensionless').units:
-            return self.FUNCS[func_name](**d)
-        else:
-            return self.FUNCS[func_name](**d)*units
-
+from nspyre.utils import RangeDict
 
 class Rangespace(QtWidgets.QWidget):
     def __init__(self, units=None, parent=None, default=None):

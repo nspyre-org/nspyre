0c948fa1734ecf409ee81160cab65f5c22567240
AlexBourassa <abourassa@uchicago.edu>
2019-11-12 11:31:05 -0600

    Added an experimental state to the config file and a function to query that state during save

--- nspyre/data_handling.py
+++ nspyre/data_handling.py
@@ -1,11 +1,49 @@
-from nspyre.utils import custom_encode, custom_decode, connect_to_master
+from nspyre.utils import custom_encode, custom_decode, connect_to_master, get_configs
 from collections import OrderedDict
 import json
 import pymongo
 import pandas as pd
+from nspyre.instrument_manager import Instrument_Manager
+import traceback
+# from lantz import Q_
 
+def gen_exp_state(debug=False):
+    state_dict = OrderedDict()
+    try:
+        m = Instrument_Manager()
+    except:
+        print("Could not start the instrument manager")
+        if debug: traceback.print_exc()
 
-def save_data(spyrelet, filename, name=None, description=None):
+    cfg = get_configs()
+
+    if 'experimental_state' in cfg and \
+        'device_feat' in cfg['experimental_state'] and \
+        not cfg['experimental_state']['device_feat'] is None:
+        for name, vals in cfg['experimental_state']['device_feat'].items():
+            try:
+                ans = getattr(m.get(vals[0])['dev'],vals[1])
+                # if type(ans) == Q_:
+                #     ans = {'__type__':'Quantity', 'm':ans.m, 'units':str(ans.units)}
+                state_dict[name] = str(ans)
+            except:
+                print("Could not save {}".format(name))
+                if debug: traceback.print_exc()
+    if 'experimental_state' in cfg and \
+        'device_dictfeat' in cfg['experimental_state'] and \
+        not cfg['experimental_state']['device_dictfeat'] is None:
+        for name, vals in cfg['experimental_state']['device_dictfeat'].items():
+            try:
+                ans = getattr(m.get(vals[0])['dev'],vals[1])[vals[2]]
+                # if type(ans) == Q_:
+                #     ans = {'__type__':'Quantity', 'm':ans.m, 'units':str(ans.units)}
+                state_dict[name] = str(ans)
+            except:
+                print("Could not save {}".format(name))
+                if debug: traceback.print_exc()
+    return state_dict
+
+def save_data(spyrelet, filename, name=None, description=None, save_state = True, debug=False):
     d = spyrelet.data.drop(['_id'], axis=1)
     data_dict = OrderedDict([
         ('dataset',name),
@@ -15,6 +53,7 @@ def save_data(spyrelet, filename, name=None, description=None):
         ('data_col', list(d.columns)),
         ('data', d.to_json(orient='values')),
     ])
+
     child_dict = OrderedDict()
     for c_name, data_list in spyrelet.child_data.items():
         child_dict[c_name] = OrderedDict([
@@ -23,12 +62,24 @@ def save_data(spyrelet, filename, name=None, description=None):
             ('data_list',[dl.drop(['_id'], axis=1).to_json(orient='values') for dl in data_list]),
         ])
     data_dict['children'] = child_dict
-    if filename is None:
-        return data_dict
-    else:
-        with open(filename, 'w') as f:
-            results = json.dump(data_dict, f)
-        return results
+
+    #This takes care of writting the experimental state
+    try:
+        state_dict = gen_exp_state(debug=debug) if save_state else {}
+    except:
+        print("Could generate state dict")
+        state_dict = dict()
+        if debug: traceback.print_exc()
+
+    #Write the file
+    finally:
+        data_dict['experimental_state'] = state_dict
+        if filename is None:
+            return data_dict
+        else:
+            with open(filename, 'w') as f:
+                results = json.dump(data_dict, f)
+            return results
 
 def load_data(filename, mongodb_addrs=None, db_name='Spyre_Data_Loaded'):
     with open(filename, 'r') as f:

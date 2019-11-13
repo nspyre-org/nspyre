56cdb8121a45d52284c03e3673506e06ad7f3182
AlexBourassa <abourassa@uchicago.edu>
2019-11-13 10:20:46 -0600

    Added a main widget for easy launching of the various components of NSpyre

---
+++ nspyre/widgets/main.py
@@ -0,0 +1,53 @@
+from PyQt5 import QtCore, QtWidgets
+from nspyre.widgets.image import ImageWidget
+from nspyre.utils import join_nspyre_path
+from subprocess import Popen, CREATE_NEW_CONSOLE
+import os
+
+
+class Spyre_Launcher(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        
+        main_layout = QtWidgets.QVBoxLayout()
+        im = ImageWidget(join_nspyre_path('images/spyre.png'))
+        main_layout.addWidget(im)
+
+        # btn_widget = QtWidgets.QWidget()
+        # btn_layout = QtWidgets.QHBoxLayout()
+
+        self.inst_server_btn = QtWidgets.QPushButton('Start Instrument Server')
+        self.inst_manager_btn = QtWidgets.QPushButton('Start Instrument Manager')
+        self.view_manager_btn = QtWidgets.QPushButton('Start View Manager')
+        self.spyrelet_launcher_btn = QtWidgets.QPushButton('Start Spyrelet Launcher')
+
+        main_layout.addWidget(self.inst_server_btn)
+        main_layout.addWidget(self.inst_manager_btn)
+        main_layout.addWidget(self.view_manager_btn)
+        main_layout.addWidget(self.spyrelet_launcher_btn)
+
+        self.inst_server_btn.clicked.connect(lambda: self.launch('instrument_server.py', new_console=True))
+        self.inst_manager_btn.clicked.connect(lambda: self.launch('widgets/instrument_manager.py'))
+        self.view_manager_btn.clicked.connect(lambda: self.launch('widgets/view_manager.py'))
+        self.spyrelet_launcher_btn.clicked.connect(lambda: self.launch('widgets/launcher.py'))
+
+        self.setLayout(main_layout)
+    
+    def launch(self, nspyre_py_file, new_console=False):
+        if new_console:
+            filename = os.path.normpath(join_nspyre_path(nspyre_py_file))
+            filename = filename.replace('\\', '/')
+            cmd = 'bash -c "activate notebook; python {}"'.format(filename)
+            Popen(cmd, creationflags=CREATE_NEW_CONSOLE)
+        else:
+            Popen(['python', join_nspyre_path(nspyre_py_file)])
+
+
+
+
+if __name__ == '__main__':
+    from nspyre.widgets.app import NSpyreApp
+    app = NSpyreApp([])
+    w = Spyre_Launcher()
+    w.show()
+    app.exec_()

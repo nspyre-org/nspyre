4ef2765ebc8e07374ee912832fee02a2dd1ea2c1
AlexBourassa <alexandre.bourassa.mail@gmail.com>
2019-12-05 18:16:38 -0600

    Centralized a few key imports for writting Spyrelets to the __init__.py

---
+++ nspyre/__init__.py
@@ -0,0 +1,5 @@
+from nspyre.views import Plot1D, Plot2D, PlotFormatInit, PlotFormatUpdate
+from nspyre.spyrelet import Spyrelet
+from nspyre.widgets.plotting import LinePlotWidget
+
+__all__ = ['Plot1D', 'Plot2D', 'PlotFormatInit', 'PlotFormatUpdate', 'Spyrelet', 'LinePlotWidget']

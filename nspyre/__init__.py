618f85930371aa94ca711684ffab727be95eb3e5
AlexBourassa <alexandre.bourassa.mail@gmail.com>
2019-12-10 14:19:37 -0600

    Added the Q_ unit registry to the standard nspyre import

--- nspyre/__init__.py
+++ nspyre/__init__.py
@@ -1,5 +1,6 @@
 from nspyre.views import Plot1D, Plot2D, PlotFormatInit, PlotFormatUpdate
 from nspyre.spyrelet import Spyrelet
 from nspyre.widgets.plotting import LinePlotWidget
+from lantz import Q_
 
-__all__ = ['Plot1D', 'Plot2D', 'PlotFormatInit', 'PlotFormatUpdate', 'Spyrelet', 'LinePlotWidget']
+__all__ = ['Plot1D', 'Plot2D', 'PlotFormatInit', 'PlotFormatUpdate', 'Spyrelet', 'LinePlotWidget', 'Q_']

a05cf20eb80b9e91b2cb30d25ea37bb158344df8
mtsolmn <6020319+mtsolmn@users.noreply.github.com>
2020-07-17 13:32:31 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ extensions = [
     #'jupyter_sphinx',
     'nbsphinx', # for .ipynb file support (i.e. jupyter notebooks)
     #'sphinxcontrib.bibtex', # for bibliographic references
-    'sphinxcontrib.rsvgconverter' # for SVG->PDF conversion in LaTeX output
+    'sphinxcontrib.rsvgconverter', # for SVG->PDF conversion in LaTeX output
     'sphinx_copybutton' # for adding 'copy to clipboard' buttons to all text/code boxes
 ]
 

3ba9a0dca2b7034701d0130a245fd632fa9ee27a
Jacob Feder <jacobsfeder@gmail.com>
2020-07-26 16:48:40 -0500

    start mongodb entirely from shell script

--- nspyre/mongodb/start_mongo_unix.sh
+++ nspyre/mongodb/start_mongo_unix.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DB1_PORT=27017
+DB2_PORT=27018
+REPLSET=NSpyreSet
+OPLOG=1024
 # may be required to start the mongodb daemon
 #systemctl start mongodb.service
 
 # start the db servers
-mongod --config mongodb1_unix.cfg --fork
-mongod --config mongodb2_unix.cfg --fork
+mongod --dbpath $DIR/db1 --logpath $DIR/logs/db1 --bind_ip_all --port $DB1_PORT --replSet $REPLSET --oplogSize $OPLOG --fork
+mongod --dbpath $DIR/db2 --logpath $DIR/logs/db2 --bind_ip_all --port $DB2_PORT --replSet $REPLSET --oplogSize $OPLOG --fork
 
 # only needs to performed for first-time setup 
 # or if the db1/db2 directories were cleared
 # add both servers to a replica set to allow them to start serving the db
 # TODO use same port as specificed in .cfg
-mongo --eval "rs.initiate({_id:'NSpyreSet', members:[ \
+mongo --eval "rs.initiate({_id:'${REPLSET}', members:[ \
 {_id: 0, host: 'localhost:27017'}, \
 {_id: 1, host: 'localhost:27018'}  \
 ]})"

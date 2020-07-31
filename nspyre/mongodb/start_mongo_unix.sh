47022d48275d085826cc933db2ce0e50ca9f6a00
Jacob Feder <jacobsfeder@gmail.com>
2020-07-30 19:49:49 -0500

    hopefully make mongo startup more robust

--- nspyre/mongodb/start_mongo_unix.sh
+++ nspyre/mongodb/start_mongo_unix.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# script for starting the mongodb server
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DB1_PORT=27017
 DB2_PORT=27018
 REPLSET=NSpyreSet
 OPLOG=1024
-# may be required to start the mongodb daemon
+# may be required to start the mongodb daemon?
 #systemctl start mongodb.service
+
+# kill existing mongod instances
+killall mongod
 
 # start the db servers
 mongod --dbpath $DIR/db1 --logpath $DIR/logs/db1 --bind_ip_all \
@@ -18,7 +23,8 @@ mongod --dbpath $DIR/db2 --logpath $DIR/logs/db2 --bind_ip_all \
 # or if the db1/db2 directories were cleared,
 # but no disadvantage of running it anyway
 # add both servers to a replica set to allow them to start serving the db
+# make db1 be the preferred primary using priorities
 mongo --eval "rs.initiate({_id:'${REPLSET}', members:[ \
-{_id: 0, host: 'localhost:${DB1_PORT}'}, \
-{_id: 1, host: 'localhost:${DB2_PORT}'}  \
+{_id: 0, host: 'localhost:${DB1_PORT}', priority: 2}, \
+{_id: 1, host: 'localhost:${DB2_PORT}', priority: 1}  \
 ]})"
--- nspyre/mongodb/start_mongo_win.bat
+++ nspyre/mongodb/start_mongo_win.bat
@@ -1,5 +1,23 @@
-mongod --config C:\mongodb\mongodb1_win.cfg
-mongod --config C:\mongodb\mongodb2_win.cfg
-net start mongodb1
-net start mongodb2
+rem script for starting the mongodb server
 
+set THIS_DIR=%~dp0
+set DB1_PORT=27017
+set DB2_PORT=27018
+set REPLSET=NSpyreSet
+set OPLOG=1024
+
+rem kill existing mongod instances
+taskkill /f /im mongod.exe
+
+rem start the db servers
+start /b mongod --dbpath %THIS_DIR%db1 --logpath %THIS_DIR%logs\db1 ^
+	--bind_ip_all --port %DB1_PORT% --replSet %REPLSET% --oplogSize %OPLOG%
+start /b mongod --dbpath %THIS_DIR%db2 --logpath %THIS_DIR%logs\db2 ^
+	--bind_ip_all --port %DB2_PORT% --replSet %REPLSET% --oplogSize %OPLOG%
+
+rem only needs to performed for first-time setup
+rem or if the db1/db2 directories were cleared,
+rem but no disadvantage of running it anyway
+rem add both servers to a replica set to allow them to start serving the db
+rem make db1 be the preferred primary using priorities
+mongo --eval "rs.initiate({_id:'%REPLSET%', members:[ {_id: 0, host: 'localhost:%DB1_PORT%', priority: 2}, {_id: 1, host: 'localhost:%DB2_PORT%', priority: 1}]})"

4e4685ecd6be7200395f665dbc0e327071d15a3f
Jacob Feder <jacobsfeder@gmail.com>
2022-02-13 13:13:16 -0600

    fix windows issue with multiprocessing and if __name__ == '__main__'

--- examples/odmr/app.py
+++ examples/odmr/app.py
@@ -18,47 +18,50 @@ from nspyre import NspyreApp
 
 HERE = Path(__file__).parent
 
-# Log to the console as well as a file inside the logs folder.
-nspyre_init_logger(
-    log_level=logging.INFO,
-    log_path=HERE / 'logs',
-    log_path_level=logging.DEBUG,
-    prefix='fancy_odmr',
-    file_size=10_000_000,
-)
+# if using the nspyre ProcessRunner, the main code must be guarded with if __name__ == '__main__':
+# see https://docs.python.org/2/library/multiprocessing.html#windows
+if __name__ == '__main__':
+    # Log to the console as well as a file inside the logs folder.
+    nspyre_init_logger(
+        log_level=logging.INFO,
+        log_path=HERE / 'logs',
+        log_path_level=logging.DEBUG,
+        prefix='fancy_odmr',
+        file_size=10_000_000,
+    )
 
-# Create Qt application and apply nspyre visual settings.
-app = NspyreApp()
+    # Create Qt application and apply nspyre visual settings.
+    app = NspyreApp()
 
-# Create the GUI.
-main_widget = MainWidget(
-    {
-        'SaveODMR': {
-            'module': gui_elements,
-            'class': 'ODMRSaveWidget',
-            'args': (),
-            'kwargs': {},
-        },
-        'ODMR': {
-            'module': gui_elements,
-            'class': 'ODMRWidget',
-            'args': (),
-            'kwargs': {},
-        },
-        'ODMR_plot': {
-            'module': gui_elements,
-            'class': 'ODMRPlotWidget',
-            'args': (),
-            'kwargs': {},
-        },
-        'ODMR_scroll_plot': {
-            'module': gui_elements,
-            'class': 'ScrollingODMRPlotWidget',
-            'args': (),
-            'kwargs': {},
-        },
-    }
-)
-main_widget.show()
-# Run the GUI event loop.
-app.exec()
+    # Create the GUI.
+    main_widget = MainWidget(
+        {
+            'SaveODMR': {
+                'module': gui_elements,
+                'class': 'ODMRSaveWidget',
+                'args': (),
+                'kwargs': {},
+            },
+            'ODMR': {
+                'module': gui_elements,
+                'class': 'ODMRWidget',
+                'args': (),
+                'kwargs': {},
+            },
+            'ODMR_plot': {
+                'module': gui_elements,
+                'class': 'ODMRPlotWidget',
+                'args': (),
+                'kwargs': {},
+            },
+            'ODMR_scroll_plot': {
+                'module': gui_elements,
+                'class': 'ScrollingODMRPlotWidget',
+                'args': (),
+                'kwargs': {},
+            },
+        }
+    )
+    main_widget.show()
+    # Run the GUI event loop.
+    app.exec()

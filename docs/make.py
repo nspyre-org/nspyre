7059aaae8f049684f6a50b345ecbab78499e5c02
Jacob Feder <jacobsfeder@gmail.com>
2022-02-13 12:41:47 -0600

    actually sphinx build on windows wasn't really working - fixed now

--- docs/make.py
+++ docs/make.py
@@ -7,12 +7,16 @@ HERE = Path(__file__).parent
 
 def main():
     if sys.platform == 'win32':
-        cmd = (f'make -C clean {HERE} && make -C {HERE} html',)
-        kwargs = {'shell': True}
+        # cmd = (f'make -C clean {HERE} && make -C {HERE} html',)
+        cmd = ['make', '-C', str(HERE), 'clean']
+        ret = subprocess.call(cmd)
+        if ret:
+            return ret
+        cmd = ['make', '-C', str(HERE), 'html']
+        return subprocess.call(cmd)
     else:
-        cmd = (f"bash -c 'make -C {HERE} clean && make -C {HERE} html'",)
-        kwargs = {}
-    return subprocess.call(cmd, **kwargs)
+        cmd = (f'bash -c \'make -C {HERE} clean && make -C {HERE} html\'',)
+        return subprocess.call(cmd)
 
 
 if __name__ == '__main__':
